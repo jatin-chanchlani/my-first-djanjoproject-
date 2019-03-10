9d790b92b0460e85045f7f9ab49a35be189db448
jatin <jatin.c@somaiya.edu>
2019-03-10 22:48:14 +0530

    Added views to create/edit blog post inside the site..

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import  get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
